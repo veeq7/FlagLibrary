5c8fae81fa8b74b3fcdec4a57ba173ab61aa7dcf
veeq07 <faficzek2424@gmail.com>
2020-02-26 13:36:20 +0100

    Sql generation

--- FlagLibrary/Generators/SqlGenerator.cs
+++ FlagLibrary/Generators/SqlGenerator.cs
@@ -7,33 +7,60 @@ using FlagLibrary.Flags;
 
 namespace FlagLibrary.Generators
 {
-    class SqlGenerator
+    public class SqlGenerator
     {
-        public string GenerateInsert(Dictionary<int, ParsedFlagData> data, string flagName)
+        private bool CheckIfBitStringIsEmpty(string bits)
         {
-            string insertString = "";
+            if (!bits.Contains('1') && !bits.Contains('?'))
+                return true;
+            return false;
+        }
 
+        public string GenerateInsert(string bits, string flagName)
+        {
+            if (CheckIfBitStringIsEmpty(bits)) return "";
+            string insertString = "Insert:\n" + flagName + "=(";
+
+            int i = 0;
+            foreach (var ch in bits)
+            {
+                if (ch == '1')
+                {
+                    insertString += "(1<<"+i+")|";
+                }
+                i++;
+            }
+            insertString.Remove(insertString.Length - 1);
+
+            insertString += ");";
             return insertString;
         }
 
-        public string GenerateUpdate(Dictionary<int, ParsedFlagData> data, string flagName)
+        public string GenerateUpdate(string bits, string flagName)
         {
-            string updateStringSet = flagName + "=" + flagName +"|";
-            string updateStringUnset = flagName + "=" + flagName + "&(~(";
+            if (CheckIfBitStringIsEmpty(bits)) return "";
+            string updateStringSet = "Update (set):\n" + flagName + "=" + flagName +"|(";
+            string updateStringUnset = "Update (remove):\n" + flagName + "=" + flagName + "&(~(";
 
-
-            foreach (KeyValuePair<int, ParsedFlagData> kv in data)
+            int i = 0;
+            foreach (var ch in bits)
             {
-                ParsedFlagData pFlag = kv.Value;
-                foreach (int bitRef in pFlag.flag.bitRefs)
+                if (ch == '1')
                 {
-
+                    updateStringSet += "(1<<" + i + ")|";
                 }
+                else if (ch == '0')
+                {
+                    updateStringUnset += "(1<<" + i + ")|";
+                }
+                i++;
             }
+            updateStringSet.Remove(updateStringSet.Length - 1);
+            updateStringUnset.Remove(updateStringUnset.Length - 1);
 
-            updateStringSet += ")";
+            updateStringSet += ")"; ;
             updateStringUnset += "))"; ;
-            return updateStringSet + "; " + updateStringUnset + ";";
+            return updateStringSet + ";\n" + updateStringUnset + ";";
         }
     }
 }
